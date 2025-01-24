850911f13c66dae5752f49f2f2e08dd28db98376
Otmane <166559635+otboumeh@users.noreply.github.com>
2025-01-24 11:14:26 +0100

    Update Actividad.java

--- ElorrietaTravels/src/main/java/app/com/models/Actividad.java
+++ ElorrietaTravels/src/main/java/app/com/models/Actividad.java
@@ -1,5 +1,59 @@
 package main.java.app.com.models;
 
 public class Actividad {
+    private String idActividad;
+    private String descripcion;
+    private String fecha;
+    private int precio;
 
+    public Actividad() {}
+
+    public Actividad(String idActividad, String descripcion, String fecha, int precio) {
+        this.idActividad = idActividad;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+
+    public String getIdActividad() {
+        return idActividad;
+    }
+
+    public void setIdActividad(String idActividad) {
+        this.idActividad = idActividad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Actividad{" +
+                "idActividad='" + idActividad + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
 }
