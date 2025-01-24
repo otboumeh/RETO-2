5d90d40b015462edaa5b554287fd32a230eaaa0b
Otmane <166559635+otboumeh@users.noreply.github.com>
2025-01-24 11:12:50 +0100

    Update PlanViaje.java

--- ElorrietaTravels/src/main/java/app/com/models/PlanViaje.java
+++ ElorrietaTravels/src/main/java/app/com/models/PlanViaje.java
@@ -1,5 +1,60 @@
 package main.java.app.com.models;
 
 public class PlanViaje {
+    private String idPlan;
+    private String trayecto;
+    private String idVueloIda;
+    private String idVueloVuelta;
 
+    public PlanViaje() {}
+
+    public PlanViaje(String idPlan, String trayecto, String idVueloIda, String idVueloVuelta) {
+        this.idPlan = idPlan;
+        this.trayecto = trayecto;
+        this.idVueloIda = idVueloIda;
+        this.idVueloVuelta = idVueloVuelta;
+    }
+
+    public String getIdPlan() {
+        return idPlan;
+    }
+
+    public void setIdPlan(String idPlan) {
+        this.idPlan = idPlan;
+    }
+
+    public String getTrayecto() {
+        return trayecto;
+    }
+
+    public void setTrayecto(String trayecto) {
+        this.trayecto = trayecto;
+    }
+
+    public String getIdVueloIda() {
+        return idVueloIda;
+    }
+
+    public void setIdVueloIda(String idVueloIda) {
+        this.idVueloIda = idVueloIda;
+    }
+
+    public String getIdVueloVuelta() {
+        return idVueloVuelta;
+    }
+
+    public void setIdVueloVuelta(String idVueloVuelta) {
+        this.idVueloVuelta = idVueloVuelta;
+    }
+
+    @Override
+    public String toString() {
+        return "PlanViaje{" +
+                "idPlan='" + idPlan + '\'' +
+                ", trayecto='" + trayecto + '\'' +
+                ", idVueloIda='" + idVueloIda + '\'' +
+                ", idVueloVuelta='" + idVueloVuelta + '\'' +
+                '}';
+    }
 }
+
